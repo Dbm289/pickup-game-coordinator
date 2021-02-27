0f1dd3be8ad026bd9e0fe84ec0115e3088cf776d
Dan <DanielMooreRealPerson@gmail.com>
2021-02-27 18:54:03 -0500

    add timestamps to courts table mig

--- db/migrate/20210227190214_create_users_table.rb
+++ db/migrate/20210227190214_create_users_table.rb
@@ -4,6 +4,13 @@ class CreateUsersTable < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :email
       t.string :password_digest
+      t.string :position
+      t.string :alt_position
+      t.integer :skill_level
+      t.integer :coaching_skill_level
+      t.integer :team_id
+
+      t.timestamps null: false
 
 
   end
