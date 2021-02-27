cd1e1e597f121abf43465b0bba756e43158dbaba
Dan <DanielMooreRealPerson@gmail.com>
2021-02-27 18:54:54 -0500

    add timestamps to courts table mig

---
+++ db/migrate/20210227233430_create_courts.rb
@@ -0,0 +1,10 @@
+class CreateCourts < ActiveRecord::Migration
+  def change
+    create_table :courts do |t|
+      t.string :name
+      t.integer :quality
+
+      t.timestamps null: false
+    end
+  end
+end
