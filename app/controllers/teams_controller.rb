c0c1d23b369449761d3da18aab0b023d2a9960dc
Dan <DanielMooreRealPerson@gmail.com>
2021-03-02 12:22:58 -0500

    adds logic to process the addition of teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,6 +4,16 @@ class TeamsController < ApplicationController
     end
 
     post '/teams' do
+        if !logged_in?
+            redirect '/'
+        end
+        if params[:name] != ""
+            @team = Team.create(params)
+            #likely need something else here
+            redirect "teams/#{@team.id}"
+        else 
+            redirect '/teams/new'
+        end
 
     end
     
